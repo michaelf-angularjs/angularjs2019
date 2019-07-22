ffc755f7b8a8c4dd191d86427ef26badf247ee44
michaelf-angularjs <53136398+michaelf-angularjs@users.noreply.github.com>
2019-07-22 11:46:33 +0300

    Add files via upload

---
+++ pclass.js
@@ -0,0 +1,11 @@
+class Pclass {
+    constructor(name, age, password,gender,car,bike)
+    {
+        this.name = name
+        this.age = age
+        this.password = password
+        this.gender = gender
+        this.car = car
+        this.bike = bike
+    }
+}
---
+++ person.js
@@ -0,0 +1,9 @@
+function Person(name, age, password,gender,car,bike)
+{
+    this.name = name
+    this.age = age
+    this.password = password
+    this.gender = gender
+    this.car = car
+    this.bike = bike
+}
